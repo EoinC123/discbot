f9d918248a3f9bdf9558d926c1d851072a5728fe
Eoin Cotter <eoinjjc@hotmail.com>
2023-08-10 16:08:41 +0100

    Update settings.py
    removed redundant code

--- settings.py
+++ settings.py
@@ -6,39 +6,7 @@ import logging
 import os
 
 PROJECT_BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-
-NAME_TO_MUSIC_MAPPING = {
-    111233508520767488: "fuck_books.mp3",  # Eoin
-    145350084983390208: "itwasmebarry.mp3",  # Ronan
-    120990968173297665: "windows-xp-startup.mp3",  # Archie
-    186588349807591434: "im-fast-as-f-boi.mp3",  # Paul
-    219639243733991424: "epic-sax-guy.mp3",  # Peter
-    100295458856931328: "08-_Fuckin_Gay.mp3",  # David A
-    223485287974699008: "jawohl.mp3",  # Dave D
-    221240253472702466: "trump-cum.mp3",  # Jason
-    292454324368572416: "build-a-wall.mp3",  # Scott
-    155807750344015872: "egel.mp3",  # Nigel
-    182658393549438986: "wide-putin.mp3",  # Darren
-    176758486410067987: "Wont_Fuck_Girls.mp3",  # Conor
-    234765588847656960: "ytug.mp3", # Olan
-    178232645832933376: "wake_up_liberal.mp3", # Kieran
-}
-
-PLAYLISTS = {
-    "gachi": [
-        # "https://www.youtube.com/watch?v=c9JNp6kdKqU&ab_channel=GachiFingers", # need to "confirm age"
-        "https://www.youtube.com/watch?v=NdqbI0_0GsM&ab_channel=flox_",
-        "https://www.youtube.com/watch?v=PFyMhNZB-lc&ab_channel=Bossofthisgym",
-        "https://www.youtube.com/watch?v=kOCxHu_F5xo&ab_channel=Cypac",
-        "https://www.youtube.com/watch?v=9Ebdpv0KOlM&ab_channel=Cypac",
-        "https://www.youtube.com/watch?v=z2OGa6RyYDE",
-    ],
-}
-
-
 EXECUTABLE_FOLDER = os.path.join(PROJECT_BASE_DIR, "executables")
-SOUND_EFFECTS_FOLDER = os.path.join(PROJECT_BASE_DIR, "sound_effects")
-
 ALLOW_PLAY = bool(strtobool(os.environ.get("ALLOW_PLAY", "True")))
 
 # define the reactions here, so they can be changed from one place
@@ -108,8 +76,6 @@ REACTIONS = {
     "diamonds": "♦",
     "clubs": "♣",
 }
-
-GAMBLING_DB = os.path.join(PROJECT_BASE_DIR, "gamble.db")
 
 # logger init
 DEBUG = False
