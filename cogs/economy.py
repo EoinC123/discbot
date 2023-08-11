1e3569b016b4dc6f6d6eef8723206980b00a4f8c
eoin-c <c_plasticmask_c@proton.me>
2023-08-11 14:53:45 -0400

    added !give command for economy, can give users moeny from own balance

--- cogs/economy.py
+++ cogs/economy.py
@@ -35,14 +35,56 @@ class economy(commands.Cog):
         db = sqlite3.connect("economy.db")
         cur = db.cursor()
         cur.execute(f'SELECT cash FROM users WHERE user_id = {user}')
-        results = cur.fetchone()[0]
+        results = cur.fetchone()
         if results is None:
             await ctx.send(f"Couldnt find wallet for {ctx.author}, try using command !create to start a wallet")
         else:
-            await ctx.send(f'{ctx.author} has {results} squarecoins')
+            await ctx.send(f'{ctx.author} has {results[0]} squarecoins')
         db.close()
 
+    # give another user some cash from your wallet
+    @commands.command()
+    async def give(self, ctx, *args):
+        try:
+            amount = int(args[0])
+        except Exception as e:
+            print("sometin fucky")
+            return
 
+        user_sender = ctx.author.id
+        user_receiever = int(args[1][2:-1])
+        db = sqlite3.connect("economy.db")
+        cur = db.cursor()
+        cur.execute(f"SELECT cash FROM users WHERE user_id = {user_sender}")
+        user_sender_bal = cur.fetchone()[0]
+        if user_sender_bal is None:
+            await ctx.send(f"You dont have a wallet {user_sender}, you can make one with command !create")
+            db.close()
+            return
+        elif user_sender_bal <= 0:
+            await ctx.send(f"You have {user_sender_bal} cash, you cant send anything to anyone.")
+            db.close()
+            return
+        else:
+            cur.execute(f'SELECT cash FROM users WHERE user_id = {user_sender}')
+            res_x = cur.fetchone()[0]
+            new_amount_x = res_x - amount
+
+            cur.execute(f'SELECT cash FROM users WHERE user_id = {user_receiever}')
+            res_y = cur.fetchone()[0]
+            new_amount_y = res_y + amount
+
+            if new_amount_x < 0:
+                await ctx.send(f'You cant give that much away')
+                db.close()
+                return
+            else:
+                cur.execute(f"UPDATE users SET cash = {new_amount_x} WHERE user_id = {user_sender}")
+                db.commit()
+                cur.execute(f'UPDATE users SET cash = {new_amount_y} WHERE user_id = {user_receiever}')
+                db.commit()
+                db.close()
+                await ctx.send(f"{ctx.author} just gave {amount} to {ctx.guild.get_member(user_receiever)}, very noice.")
 
 
 async def setup(client):
