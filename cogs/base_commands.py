8fcda0ed12529afda976af04ea3fc317f8eb32af
Artsiom Vahin <artsiomv1@gmail.com>
2023-08-12 23:32:36 +0100

    removing unneccesary files, updating gitignore, adding basic music for windows

--- cogs/base_commands.py
+++ cogs/base_commands.py
@@ -13,6 +13,14 @@ class base_commands(commands.Cog):
     async def ping(self, ctx):
         await ctx.send(f'Latency is currently {round(self.client.latency * 1000)}ms')
 
+    # @commands.command()
+    # async def dc(self, ctx):
+    #     for vc in self.client.voice_clients:
+    #         if (vc.server == ctx.message.server):
+    #             return await vc.disconnect()
+
+        return await self.client.say("I am not connected to any voice channel on this server!")
+
 
 async def setup(client):
     await client.add_cog(base_commands(client))
